66da6b71d48d78aa507976e15ddec1ddabcff5b4
unknown <ophantomo990@gmail.com>
2025-04-16 10:32:03 +0100

    'added-auto'

--- src/lib/components/auth/Google.svelte
+++ src/lib/components/auth/Google.svelte
@@ -3,15 +3,16 @@
   import { goto } from "$app/navigation";
   import { loginUser } from "../../auth";
   import Toast from "$lib/components/shared/Toast.svelte";
-    let msg = $state();
-    let type = $state();
+  let msg = $state();
+  let type = $state();
 
-    function triggerToast(type_arg, msg_arg) {
-        msg = msg_arg;
-        type = type_arg;
-    }
+  function triggerToast(type_arg, msg_arg) {
+    msg = msg_arg;
+    type = type_arg;
+  }
 
   let isLoading = $state(false);
+  let ready = $state(false);
 
   // Replace with your actual Google Client ID
   const GOOGLE_CLIENT_ID =
@@ -40,7 +41,7 @@
     } catch (error) {
       isLoading = false;
       console.error("Error verifying Google token:", error);
-      triggerToast('error', 'An error occured')
+      triggerToast("error", "An error occured");
       throw error;
     }
   }
@@ -97,12 +98,12 @@
         loginResponse.access,
         loginResponse.refresh,
         loginResponse.profile,
-        loginResponse.register
+        loginResponse.register,
       );
     } catch (error) {
       isLoading = false;
       console.error("Error during Google login:", error);
-      triggerToast('error', error)
+      triggerToast("error", error);
     }
   }
 
@@ -127,46 +128,55 @@
       if (typeof google !== "undefined") {
         clearInterval(checkGoogleLibrary);
         initializeGoogleSignIn();
+        ready = true;
       }
     }, 100); // Check every 100ms
   });
 </script>
+
 {#if msg}
-    <Toast bind:message={msg} {type} />
+  <Toast bind:message={msg} {type} />
 {/if}
 <div>
   <button
     onclick={handleGoogleSignIn}
+    disabled={!ready}
     class="mx-auto w-[90%] md:w-full h-12 bg-[#ccfc7e] font-extrabold text-black rounded-md flex items-center px-2 active:scale-95 transition-all"
->
+  >
     {#if !isLoading}
-        <div class="flex-shrink-0 mr-auto rounded-md p-2 bg-black">
-            <svg class="w-5 h-5" viewBox="0 0 24 24">
-                <g stroke="none" stroke-width="0.5" fill="none">
-                    <path
-                        d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z"
-                        fill="#4285F4"
-                    />
-                    <path
-                        d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z"
-                        fill="#34A853"
-                    />
-                    <path
-                        d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z"
-                        fill="#FBBC05"
-                    />
-                    <path
-                        d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z"
-                        fill="#EA4335"
-                    />
-                </g>
-            </svg>
-        </div>
-        <span class="flex-grow text-center flot-b">Continue with Google</span>
+      <div class="flex-shrink-0 mr-auto rounded-md p-2 bg-black">
+        <svg class="w-5 h-5" viewBox="0 0 24 24">
+          <g stroke="none" stroke-width="0.5" fill="none">
+            <path
+              d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z"
+              fill="#4285F4"
+            />
+            <path
+              d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z"
+              fill="#34A853"
+            />
+            <path
+              d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z"
+              fill="#FBBC05"
+            />
+            <path
+              d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z"
+              fill="#EA4335"
+            />
+          </g>
+        </svg>
+      </div>
+      <span class="flex-grow text-center flot-b"
+        >{#if !ready}
+          <span class="loader"></span>
+        {:else}
+          Continue with Google
+        {/if}</span
+      >
     {:else}
-        <span class="loader"></span>
+      <span class="loader"></span>
     {/if}
-</button>
+  </button>
 </div>
 
 <svelte:head>
