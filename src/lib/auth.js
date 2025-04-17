f8c4a2453989d5e8d4695f5d641529e44fe8e865
unknown <ophantomo990@gmail.com>
2025-04-17 13:08:37 +0100

    'mango'

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -25,7 +25,7 @@ export const setTokens = (access, refresh) => {
     path: '/',
     expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000)
   });
-  setCookie('mango', 'berry');
+  setCookie('mango', 'berry', 1);
 };
 
 export const clearTokens = () => {
